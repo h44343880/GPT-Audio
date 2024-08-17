9c329b375a34affdd652d5e56d84bd778586a176
andrewhsugithub <andrew1mail@gmail.com>
2024-08-17 21:50:51 +0800

    add logger (wip)

---
+++ src/logger.py
@@ -0,0 +1,12 @@
+﻿import logging
+
+class Logger:
+    def __init__(self):
+        self.log = []
+
+    def log(self, message):
+        self.log.append(message)
+
+    def print_log(self):
+        for message in self.log:
+            print(message)
